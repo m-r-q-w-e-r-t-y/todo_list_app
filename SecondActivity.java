178f6281d034b8e8f5456a0d1c8714ede0403562
m-r-q-w-e-r-t-y <53050066+m-r-q-w-e-r-t-y@users.noreply.github.com>
2020-04-17 22:26:16 -0400

    Update SecondActivity.java

--- SecondActivity.java
+++ SecondActivity.java
@@ -1,1 +1,74 @@
+package com.example.recycleview;
 
+import android.content.Context;
+import android.content.Intent;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
+{
+
+    String data1[], data2[];
+    Context context;
+
+    public MyAdapter(Context ct, String s1[], String s2[])
+    {
+        data1 = s1;
+        data2 = s2;
+        context = ct;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SecondActivity.class);
+                intent.putExtra("data1", data1[position]);
+                intent.putExtra("data2", data2[position]);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView myText1, myText2;
+        ImageView myImage;
+
+        ConstraintLayout mainLayout;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.MyText1);
+            myText2 = itemView.findViewById(R.id.MyText2);
+            myImage = itemView.findViewById(R.id.myImageView);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
